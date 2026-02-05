868aad4fa93f27e02fc5c54b6e37c28ad75dca5b
demonWare <dagostinoalessandro18@proton.me>
2026-02-05 20:59:54 +0100

    terminato connessione API tmdb

--- script.js
+++ script.js
@@ -13,10 +13,21 @@ function returnMovies(url){
         console.log(data.results)
         data.results.forEach(element => {
             const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
+
             const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+
             const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
+
             const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
             const title = document.createElement('h3');
+            title.setAttribute('class', 'title');
+
             const center = document.createElement('center');
 
             title.innerHTML = `${element.title}`;
@@ -35,7 +46,7 @@ function returnMovies(url){
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     main.innerHTML = '';
-    
+
     const searchTerm = search.value;
     if(searchTerm) {
         returnMovies(SEARCHAPI + searchTerm);
