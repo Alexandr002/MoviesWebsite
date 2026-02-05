49f2272ebccc1f2341429dd8a49a4ec9c6dd345d
demonWare <dagostinoalessandro18@proton.me>
2026-02-05 20:52:02 +0100

    realizzazione script per API movieTB. Ricerca film tramite barra di ricerca

---
+++ script.js
@@ -0,0 +1,44 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cd48c813c1ca613a4469b1e0c072ad5f&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=cd48c813c1ca613a4469b1e0c072ad5f&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+returnMovies(APILINK);
+function returnMovies(url){
+    fetch(url).then(res =>res.json())
+    .then(function(data) {
+        console.log(data.results)
+        data.results.forEach(element => {
+            const div_card = document.createElement('div');
+            const div_row = document.createElement('div');
+            const div_column = document.createElement('div');
+            const image = document.createElement('img');
+            const title = document.createElement('h3');
+            const center = document.createElement('center');
+
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+            main.appendChild(div_row);
+        });
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+    
+    const searchTerm = search.value;
+    if(searchTerm) {
+        returnMovies(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+});
